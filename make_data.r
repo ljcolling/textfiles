0b916d79ba48ce3997a4c1b0c529decccdfa4567
Lincoln Colling <lincoln@colling.net.nz>
2019-11-13 15:33:10 +0000

    Update make_data.r

--- make_data.r
+++ make_data.r
@@ -1,1 +1,9 @@
-cat("print hello")
+#---- CREATING SOME DATA ----- #
+suppressMessages(library(tidyverse))
+set.seed(1)
+id = paste0("s",sprintf(fmt = "%.2d", seq(1,40)))
+group = runif(40) %>% map_chr(function(x) ifelse(x < .5, "group_01","group_02"))
+congru = rnorm(40, 800, sd = 20)
+incong = rnorm(40, 1000, sd = 20)
+tibble(id = id, group = group, incong = incong, congru = congru) %>% gather(condition, value = rt, -group,-id) %>% arrange(id) -> sample_tib
+#---- FINISHED CREATING DATA ----- #
